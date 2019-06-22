6b4c0e20446ac391d0c0f5cbcd4b5d1bfa9d902a
Jon Benson <hastarin@gmail.com>
2019-06-22 12:08:05 +1000

    Various updates to get to 0.91 since 0.92 upgrade appears to be breaking things.

--- upgrade.sh
+++ upgrade.sh
@@ -2,4 +2,5 @@
 sudo systemctl stop home-assistant.service
 echo "source /srv/homeassistant/bin/activate"
 echo "pip3 install --upgrade homeassistant"
+echo "hass --script check_config"
 echo "sudo systemctl start home-assistant.service"
