9304270f11d6e7cfbe4425cceefad2e665d21fba
Jon Benson <hastarin@gmail.com>
2019-07-21 00:53:12 +1000

    Updated to 0.96.1

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo systemctl stop home-assistant.service
 echo "source /srv/homeassistant/bin/activate"
-echo "pip3 install --upgrade homeassistant"
+echo "pip3 install --upgrade homeassistant==0.96.1"
 echo "hass --script check_config"
 echo "sudo systemctl start home-assistant.service"
