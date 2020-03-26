f2e81a16f0d2bc13c5c2d84721ba47c12cae1ebc
Jon Benson <hastarin@gmail.com>
2020-03-26 16:33:14 +1100

    Updates since I haven't done one in a while.

--- cleanmqtt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "cleaning " $1 " :: usage: cleanmqtt <host>"
-mosquitto_sub -h $1 -t "#" -v -u pi -P B4PDV9EQggo8DVNKtuC5 | while read line _; do mosquitto_pub -u pi -P B4PDV9EQggo8DVNKtuC5 -h $1 -t "$line" -r -n; done
